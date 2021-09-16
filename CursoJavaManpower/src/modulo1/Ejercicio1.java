fea2f443a2506602bed960303438a076ba24371c
danilo-17 <djesus@eepmad.com>
2021-09-16 15:58:19 +0200

    apartados

--- CursoJavaManpower/src/modulo1/Ejercicio1.java
+++ CursoJavaManpower/src/modulo1/Ejercicio1.java
@@ -1,9 +1,17 @@
 package modulo1;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Hello World!!");
+		System.out.println("Ahora vienen lugares hacia abajo");
+		System.out.println("\n\n\ntres lugares hacia abajo");
+		System.out.println("constante\t\tvalor");
+		System.out.println("\nPI\t\t\t3.1416");
 
 	}
 
