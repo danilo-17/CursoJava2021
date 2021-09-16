457e0a429e90e1193bc04572a6f611665fdc2528
danilo-17 <djesus@eepmad.com>
2021-09-16 16:29:48 +0200

    ejercicio 2

---
+++ CursoJavaManpower/src/modulo1/Ejercicio3.java
@@ -0,0 +1,21 @@
+package modulo1;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		byte bmin = -128;
+		byte bmax = 127;
+		short smin = 0;
+		short smax = 0;
+		int imax = 0;
+		int imin = 0;
+		long lmin = 0;
+		long lmax = 0;
+
+		System.out.println("Tipo de dato \t\t\t\t Minimo \t\t\t Maximo");
+		System.out.println("------ \t\t\t\t\t ------ \t\t\t ------");
+		System.out.println("Byte \t\t\t\t\t " + bmin + "\t\t\t\t " + bmax);
+		
+	}
+
+}
