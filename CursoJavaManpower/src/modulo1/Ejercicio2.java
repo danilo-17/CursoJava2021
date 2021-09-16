e732d48339a1c5a01b52654266c2bc82861e95cb
danilo-17 <djesus@eepmad.com>
2021-09-16 16:16:44 +0200

    ejercicio 2

---
+++ CursoJavaManpower/src/modulo1/Ejercicio2.java
@@ -0,0 +1,18 @@
+package modulo1;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		
+		System.out.println("Nombre \t\t\t\t Apellido \t\t\t Email \t\t\t\t\t Telefono");
+		System.out.println("------ \t\t\t\t ------ \t\t\t ------ \t\t\t\t ------");
+		System.out.println("Luis \t\t\t\t Navarro \t\t\t luis@gmail.com \t\t\t 685487514");
+		System.out.println("Geovanny \t\t\t Calle \t\t\t\t geovanny@gmail.com \t\t\t 685487584");
+		System.out.println("Sebastian \t\t\t Riojo \t\t\t\t sebastian@gmail.com \t\t\t 685487754");
+
+	}
+
+}
