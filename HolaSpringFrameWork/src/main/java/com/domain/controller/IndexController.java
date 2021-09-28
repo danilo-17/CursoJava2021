789f321edf1cb084caaa5524b1653a2128012fb8
danilo-17 <djesus@eepmad.com>
2021-09-28 11:42:26 +0200

    pratica 11 spring

---
+++ HolaSpringFrameWork/src/main/java/com/domain/controller/IndexController.java
@@ -0,0 +1,41 @@
+package com.domain.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/home")
+	public String goIndex() {
+		
+		System.out.println("Pase por aqui");
+		
+		return "index";
+	}
+	
+	@RequestMapping("/")
+	public String getPresentacion() {
+		
+		System.out.println("Pase por aqui");
+		
+		return "Presentacion";
+	}
+	
+	@RequestMapping("/listado")
+	public String goListado(Model model) {
+		List<String> alumnos = new ArrayList<String>();
+		alumnos.add("Juan");
+		alumnos.add("Pedro");
+		alumnos.add("José");
+		model.addAttribute("titulo", "Listado de alumnos");
+		model.addAttribute("profesor", "Gabriel Casas");
+		model.addAttribute("alumnos", alumnos);
+		return "Listado";
+	}
+
+}
